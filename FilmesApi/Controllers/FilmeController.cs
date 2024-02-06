b07d323f46750fdd7fd0ce0bad0bb634cadcb5f4
Fábio Nunes <fabionunesbr@gmail.com>
2024-02-05 22:27:45 -0300

    atualização

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -10,11 +10,17 @@ public class FilmeController: ControllerBase
     private static List<Filme> filmes = new List<Filme>();
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)
+    public void AdicionarFilme([FromBody] Filme filme)
     {
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
         Console.WriteLine("------------");
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilmes()
+    {
+        return filmes;
     }
 }
