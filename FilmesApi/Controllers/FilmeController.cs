a0521cdcf00158130a715445386c3e6effa76503
Fábio Nunes <fabionunesbr@gmail.com>
2024-02-08 21:45:41 -0300

    Atualização

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -8,19 +8,28 @@ namespace FilmesApi.Controllers;
 public class FilmeController: ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionarFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Duracao);
-        Console.WriteLine("------------");
+
+        Console.WriteLine($"Id: {filme.Id} - Titulo: {filme.Titulo} - Duração: {filme.Duracao}");
+
     }
 
     [HttpGet]
     public IEnumerable<Filme> RecuperarFilmes()
     {
         return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperarFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        return filme;
     }
 }
