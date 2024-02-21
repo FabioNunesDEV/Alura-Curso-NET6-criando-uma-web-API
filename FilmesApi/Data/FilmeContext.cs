b71ad6b39619930f8f5d554f2e86a8633e80f885
Fábio Nunes <fabionunesbr@gmail.com>
2024-02-21 20:50:02 -0300

    atualização

---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,14 @@
+﻿using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data;
+
+public class FilmeContext:DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> opts):base(opts)
+    {
+        
+    }
+
+    DbSet<Filme> Filmes { get; set; }
+}
--- FilmesApi/Program.cs
+++ FilmesApi/Program.cs
@@ -1,8 +1,16 @@
+using FilmesApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Definido conexão com o banco de dados
+var connectionString = builder.Configuration.GetConnectionString("FilmesConnection");
+
+builder.Services.AddDbContext<FilmeContext>(opts => opts.UseMySql (connectionString, ServerVersion.AutoDetect(connectionString)));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
